184302a3e6d34b4cf5c62af446e432da505a123c
Felipe Azevedo <felipe.azevedoit@outlook.com>
2023-10-02 12:00:56 -0300

    FEAT(RateLimiterPro.Infrastructure.Queue) --> Add class: AsyncRequestQueue.cs

---
+++ src/IPRateControl/RateLimiterPro.Infrastructure/Queue/AsyncRequestQueue.cs
@@ -0,0 +1,31 @@
+﻿using RateLimiterPro.Domain.Models;
+using System.Collections.Concurrent;
+
+namespace RateLimiterPro.Infrastructure.Queue
+{
+    public class AsyncRequestQueue
+    {
+        private readonly ConcurrentQueue<RequestModel> requestQueue = new();
+        private readonly SemaphoreSlim semaphore = new(3);
+        public async Task EnqueueRequestAsync(RequestModel request)
+        {
+            requestQueue.Enqueue(request);
+            await semaphore.WaitAsync();
+
+            public async Task<RequestModel> DequeueRequestAsync()
+            {
+                if (requestQueue.TryDequeue(out var request))
+                {
+                    return request;
+                }
+
+                return null;
+            }
+
+            public void ReleaseSlot()
+            {
+                semaphore.Release();
+            }
+        }
+    }
+}
