c0caa0175ba58d28a8b3fea7d6661427ac85624e
Felipe Azevedo <felipe.azevedoit@outlook.com>
2023-10-02 11:40:13 -0300

    FEAT(Domain.Interfaces) --> Add: RequestRepository.cs and IIPRecordRepository

---
+++ src/IPRateControl/RateLimiterPro.Domain/Interfaces/IIPRecordRepository.cs
@@ -0,0 +1,10 @@
+﻿using RateLimiterPro.Domain.Models;
+
+namespace RateLimiterPro.Domain.Interfaces
+{
+    public interface IIPRecordRepository
+    {
+        IPRecord AddOrUpdate(IPRecord ipRecord);
+        IPRecord GetByIpAddress(string ipAddress);
+    }
+}
---
+++ src/IPRateControl/RateLimiterPro.Domain/Interfaces/IRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using RateLimiterPro.Domain.Models;
+
+namespace RateLimiterPro.Domain.Interfaces
+{
+    public interface IRequestRepository
+    {
+        Request Add(Request request);
+        Request GetById(int id);
+        IEnumerable<Request> GetByIpAddress(string ipAddress);
+        void Update(Request request);
+    }
+}
