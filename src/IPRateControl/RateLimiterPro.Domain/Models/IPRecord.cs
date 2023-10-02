b70c8ea2d839ec02c1661465aaba42ed6d7066f1
Felipe Azevedo <felipe.azevedoit@outlook.com>
2023-10-02 11:37:16 -0300

    FEAT(Domain.Models) --> add: IPRecord.cs, Request.cs

---
+++ src/IPRateControl/RateLimiterPro.Domain/Models/IPRecord.cs
@@ -0,0 +1,10 @@
+﻿namespace RateLimiterPro.Domain.Models
+{
+    public class IPRecord
+    {
+        public string IpAddress { get; set; } 
+        public int RequestCount { get; set; } 
+    }
+}
+
+
---
+++ src/IPRateControl/RateLimiterPro.Domain/Models/Request.cs
@@ -0,0 +1,10 @@
+﻿namespace RateLimiterPro.Domain.Models
+{
+    public class Request
+    {
+        public int Id { get; set; } 
+        public string IpAddress { get; set; } 
+        public DateTime ReceivedTime { get; set; } 
+        public DateTime ProcessedTime { get; set; }
+    }
+}
