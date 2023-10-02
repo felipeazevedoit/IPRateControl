cf2ccb86a729ed397b7004c61c4e755834e3ce6e
Felipe Azevedo <felipe.azevedoit@outlook.com>
2023-10-02 11:46:02 -0300

    FEAT(Domain.Repository) --> implementing initial repository structure: PRecordRepository.cs and RequestRepository.cs;

---
+++ src/IPRateControl/RateLimiterPro.Domain/Repositories/IPRecordRepository.cs
@@ -0,0 +1,36 @@
+﻿using RateLimiterPro.Domain.Interfaces;
+using RateLimiterPro.Domain.Models;
+
+namespace RateLimiterPro.Domain.Repositories
+{
+    public class IPRecordRepository : IIPRecordRepository
+    {
+        private Dictionary<string, IPRecord> ipRecordStore = new Dictionary<string, IPRecord>();
+
+        public IPRecord AddOrUpdate(IPRecord ipRecord)
+        {
+            if (ipRecordStore.ContainsKey(ipRecord.IpAddress))
+            {
+                // Atualizar o registro de IP existente
+                ipRecordStore[ipRecord.IpAddress].RequestCount = ipRecord.RequestCount;
+            }
+            else
+            {
+                // Adicionar um novo registro de IP
+                ipRecordStore[ipRecord.IpAddress] = ipRecord;
+            }
+
+            return ipRecord;
+        }
+
+        public IPRecord GetByIpAddress(string ipAddress)
+        {
+            if (ipRecordStore.ContainsKey(ipAddress))
+            {
+                return ipRecordStore[ipAddress];
+            }
+
+            return null; // IP não encontrado
+        }
+    }
+}
---
+++ src/IPRateControl/RateLimiterPro.Domain/Repositories/RequestRepository.cs
@@ -0,0 +1,38 @@
+﻿using RateLimiterPro.Domain.Interfaces;
+using RateLimiterPro.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RateLimiterPro.Domain.Repositories
+{
+    public class RequestRepository : IRequestRepository
+    {
+        private List<Request> requestStore = new List<Request>();
+        private int nextId = 1;
+
+        public Request Add(Request request)
+        {
+            request.Id = nextId++;
+            requestStore.Add(request);
+            return request;
+        }
+
+        public Request GetById(int id)
+        {
+            return requestStore.Find(request => request.Id == id);
+        }
+
+        public IEnumerable<Request> GetByIpAddress(string ipAddress)
+        {
+            return requestStore.FindAll(request => request.IpAddress == ipAddress);
+        }
+
+        public void Update(Request request)
+        {
+            // Implementar lógica para atualização, se necessário
+        }
+    }
+}
